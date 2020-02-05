67dffbe7e56867209befeace5490a8f60852ba8c
Sean Mei <seanmei0728@gmail.com>
2020-02-05 09:11:23 -0500

    Home Page

--- front-end-files/western_ai/src/GetInvolved/GetInvolved.js
+++ front-end-files/western_ai/src/GetInvolved/GetInvolved.js
@@ -1,8 +1,12 @@
 import React from "react";
-import nl2br from 'react-newline-to-break';
 import './GetInvolved.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import styled from 'styled-components'
+import{Row,Col} from 'react-bootstrap';
+import SignUp from '../SignUpPage/SignUp';
+import { Button} from 'react-bootstrap';
+import {BrowserRouter as Router, Switch, Route, NavLink} from 'react-router-dom';
+
+
 
 
 
@@ -10,96 +14,70 @@ class Involved extends React.Component{
 
     
     render(){
-        const title = 'Get Involved';
-
-        const heading1 = 'ARE YOU INTERESTED IN EDUCATIONALS?';
-        const heading2 = 'ARE YOU INTERESTED IN PROJECTS?';
-        const heading3 = 'ARE YOU A SPONSOR?';
-      
-  
-      
-  
-        const text='Are you interested in artificial intelligence? Would you like to work with our organization in any capacity? Is red a colour? ';
-        const textb =' If you said ‘yes’ to any of those questions, you’re the perfect match for our organization! Connect with us using one of the options below and we’ll be in touch with you.';
-        const text1= 'Looking to launch your career into the field of artificial intelligence? ' 
-        const text1b= 'Join our organization to capitalize on our tutorials and opportunities to kickstart your journey to the AI industry! ' ; 
-        const text2= 'Want to develop cutting edge AI-based projects on campus? ';
-        const text2b =' Project teams give students the opportunity develop and apply machine learning skills. Apply for project teams to unlock you career in technology!'; 
-        const text3= 'Are you interested in sponsoring a student-run, driven organization passionate about AI?';
-        const text3b =  'We’d love to have your support and increase your brand exposure.';
-
+   
 
 
  
 
         return (
             <div>
-                {/*
-                <div class ='title'>{title}</div>
-                <div class ='InvPage'>
-                    <div class='top'>{texta}<br></br>{texta1}<br></br>{texta2}<br></br><br></br>{texta3}<br></br>{texta4} </div>
-                    <div class='bottom1'>{text1A}<br></br>{text1B}<br></br><br></br>{text1C}<br></br>{text1D}<br></br>{text1E} </div>
-                    <div class='bottom2'>{text2A}<br></br>{text2B}<br></br><br></br>{text2C}<br></br>{text2D}<br></br>{text2E}<br></br>{text2F} </div>
-                    <div class='bottom3'>{text3A}<br></br>{text3B}<br></br>{text3C}<br></br><br></br>{text3D}<br></br>{text3E} </div>
-                </div>
-                <div class='header'>
-                    <div class='H1'>{heading1}</div>
-                    <div class='H2'>{heading2}</div>
-                    <div class='H3'>{heading3}</div>
-                </div>
-                <Button1 type>Sing Up</Button1>
-                <Button2 type> Apply Now</Button2>
-                <Button3 type> Sponsorship Package</Button3>
-                */}
-                    
-                    <div class="container-1">
+    
+              
+                <div class="table-responsive">  
+                    <table class="center"> 
+                        <tr width="70%">
+                            <td colspan="5" class="title">Get Involved</td>
+                            <td >   
+                                <td>                            
+                                <tr>Are you interested in artificial intelligence?</tr>
+                                <tr>Would you like to work with our organization in any capacity?</tr>
+                                </td>
+                                
+                                <tr> Is red a colour?</tr>
+                                <tr>If you said ‘yes’ to any of those questions, you’re the perfect match for our organization!</tr>
+                                <tr>Connect with us using one of the options below and we’ll be in touch with you.</tr>
+                             
+                             </td>
+                        </tr>
+                    </table>
 
-                    <div class ="box-1">               
-                            <p> {title}</p>
-                            
-                    </div> 
-                    <div class ="box-2">
-                            <h3> {text}</h3>        
-                            <h3> {textb}</h3>         
-                    </div> 
-                    </div>
+                         
+                            <table class="center" >           
+                                <td>
+                                    <table> 
+                                       
+                                        <td class = "heading">ARE YOU INTERESTED IN EDUCATIONALS</td>
+                                 
+                                        <tr><td class="space">Looking to launch your career into the field of artificial intelligence?</td></tr>
+        
+                                        <tr><td>Join our organization to capitalize on our tutorials and opportunities to kickstart your journey to the AI industry!</td></tr>
+                                        <a href="" class="button3">Sign Up </a>                               
+                                    </table>
+                                </td>
+                                <td>
+                                    <table>
+                                        <td class = "heading">ARE YOU INTERESTED IN PROJECTS</td>
+                                        <tr><td class="space">Want to develop cutting edge AI-based projects on campus?</td> </tr>
+                                        <tr><td>Project teams give students the opportunity develop and apply machine learning skills. Apply for project teams to unlock you career in technology!</td></tr>
+                                        <td><a href="" class="button3">Apply Now </a></td>
+                                        
+                                        
+                                    </table>
+                                </td>
+                                <td>
+                                    <table>
+                                        <td class = "heading">ARE YOU A SPONSOR</td>
+                                        <tr><td class="space">Are you interested in sponsoring a student-run, driven organization passionate about AI?</td></tr>
+                                        <tr><td>We’d love to have your support and increase your brand exposure.</td></tr>
+                                        <a href="Sponsor-Package" class="button3">Sponsorship Package</a>
+                                        
 
-                    <div class="space"></div>
-                    <div class="container-2">
-                        <div class ="box-3">
-                            <h3> {heading1}</h3>
-                        </div>
-                        <div class ="box-3">
-                            <h3> {heading2}</h3>
-                        </div>
-                        <div class ="box-3">
-                            <h3> {heading3}</h3>
-                        </div>
-           
-                    </div>
-
-                    <div class="container-3">
-                        <div class ="box-4">
-                            <h3> {text1}</h3>
-                            <h3> {text1b}</h3>
-                            <a href="Sign-Up-Page" class="button3">Sign Up</a>
-                        </div>
-                        <div class ="box-4">
-                            <h3> {text2}</h3>
-                            <h3> {text2b}</h3>
-                            <a href="Application" class="button3">Apply Now </a>
-                        </div>
-                        <div class ="box-4">
-                            <h3> {text3}</h3>
-                            <h3> {text3b}</h3>
-                            <a href="Sponsor-Package" class="button3">Sponsorship Package</a>
-                           
-                        </div>
+                                    </table>
+                                </td>
+                            </table>
                 
-                    </div>
-
                 </div>
-            
+            </div> 
         );
     }
 }
