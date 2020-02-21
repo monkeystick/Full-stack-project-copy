2fb77f0a9cd39ca508b91dcc8812e2352ec616ef
malmasr7 <35276614+malmasr7@users.noreply.github.com>
2020-02-21 00:00:05 -0500

    refactored the file

--- front-end-files/western_ai/src/SignUpPage/SignUp.js
+++ front-end-files/western_ai/src/SignUpPage/SignUp.js
@@ -46,85 +46,79 @@ class SignUp extends React.Component {
   render(){
   return (
     <div class="SignUp">
-    <Form onSubmit = {this.handleSubmit}>
-  <div class="container">
-    <div class = "SignUpTitle">
-      <h2 class="h2-signup">Sign Up</h2>
-      </div>
-    
-      <Col>
-    <h5 class="h5-signup">Enter Your Email Address</h5>
-    <input type="email" placeholder="Email Address" email={this.state.email} onChange={this.handleEmail} required/>
-    </div>
-      </Col>
-      <Col>
-    <h5 class="h5-signup">Enter Your Full Name</h5 >
-    <input type="text" placeholder="Full Name" name={this.state.name} onChange={this.handleName} required pattern="^[a-zA-Z][a-zA-Z\s]*$"/>
-    </div>
-      </Col>
-    
-    
-      <Col>
-    <h5 class="h5-signup">Enter Your Program</h5 >
-    <input type="text" placeholder="Program" program ={this.state.program} onChange={this.handleProgram}  required pattern="^[a-zA-Z][a-zA-Z\s]*$"/>
-    </div>
-    </Col>
-    <Col>
-    <h5 class="h5-signup">Enter Your Year</h5 >
-      <div class = "year">
-      <select onChange={this.handleYear} required>
-        <option value="">Select Here</option>
-        <option value="First Year">First Year</option>  
-        <option value="Second Year">Second Year</option>  
-        <option value="Third Year">Third Year</option>  
-        <option value="Fourth Year">Fourth Year</option>
-      </select>
-      </div>
-      </div>
-      </Col>
-    
-      
-      
-        <Col>
-    <h5 class="h5-signup">Why Do You Want To Join Western AI?</h5>
-    <div>
-        <input type="checkbox" value="Educationals"/>
-        Educationals
-      </div>
-      <div>
-        <input type="checkbox" value="Project Teams"/>
-        Project Teams
-      </div>
-      <div>
-        <input type="text" placeholder = "Other"/>
-      </div>
-      </div>
-      </Col>
-      <Col>
-      <h5 class="h5-signup">Familiarity With AI</h5 >
-      <div class = "experience">
-      <select onChange={this.handleFam} required>
-        <option value="">Select Here</option>
-        <option value = "Never Heard of AI">Never Heard of AI</option>  
-        <option value = "Know of AI">Know of AI</option>  
-        <option value = "Some Experience with AI">Some Experience with AI</option>  
-        <option value = "Well Experienced with AI">Well Experienced with AI</option>
-      </select>
-      </div>
-      </div>
-      </Col>
-    
+      <Form onSubmit = {this.handleSubmit}>
+        <div class="container">
+          <div class = "SignUpTitle">
+            <h2 class="h2-signup">Sign Up</h2>
+          </div>
+          
+          <Col>
+            <h5 class="h5-signup">Enter Your Email Address</h5>
+            <input type="email" placeholder="Email Address" email={this.state.email} onChange={this.handleEmail} required/>
+          </Col>
+          <Col>
+            <h5 class="h5-signup">Enter Your Full Name</h5 >
+            <input type="text" placeholder="Full Name" name={this.state.name} onChange={this.handleName} required pattern="^[a-zA-Z][a-zA-Z\s]*$"/>
+          </Col>
+          
+          
+          <Col>
+              <h5 class="h5-signup">Enter Your Program</h5 >
+              <input type="text" placeholder="Program" program ={this.state.program} onChange={this.handleProgram}  required pattern="^[a-zA-Z][a-zA-Z\s]*$"/>
+          </Col>
+          <Col>
+            <h5 class="h5-signup">Enter Your Year</h5 >
+            <div class = "year">
+              <select onChange={this.handleYear} required>
+                <option value="">Select Here</option>
+                <option value="First Year">First Year</option>  
+                <option value="Second Year">Second Year</option>  
+                <option value="Third Year">Third Year</option>  
+                <option value="Fourth Year">Fourth Year</option>
+              </select>
+            </div>
+          </Col>
+          
+            
+            
+              <Col>
+                <h5 class="h5-signup">Why Do You Want To Join Western AI?</h5>
+              <div>
+                <input type="checkbox" value="Educationals"/>
+                Educationals
+              </div>
+              <div>
+                <input type="checkbox" value="Project Teams"/>
+                Project Teams
+              </div>
+              <div>
+                <input type="text" placeholder = "Other"/>
+              </div>
+            </Col>
+            <Col>
+              <h5 class="h5-signup">Familiarity With AI</h5 >
+              <div class = "experience">
+                <select onChange={this.handleFam} required>
+                  <option value="">Select Here</option>
+                  <option value = "Never Heard of AI">Never Heard of AI</option>  
+                  <option value = "Know of AI">Know of AI</option>  
+                  <option value = "Some Experience with AI">Some Experience with AI</option>  
+                  <option value = "Well Experienced with AI">Well Experienced with AI</option>
+                </select>
+              </div>
+            </Col>
+          
 
-      <div class = "buttonLoc">
-    <button type="submit">Submit</button>
-    </div>
+          <div class = "buttonLoc">
+            <button type="submit">Submit</button>
+          </div>
 
-  </div>
-  </Form>
+        </div>
+      </Form>
 
     </div>
-  );
-}
+    );
+  }
 }
 
 export default SignUp;
